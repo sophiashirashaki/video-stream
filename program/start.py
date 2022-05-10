c445e28a4d621de38a3b65cc2d708b4d8a4641ec
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-05-11 06:34:32 +0800

    Update start.py

--- program/start.py
+++ program/start.py
@@ -1,3 +1,31 @@
+from datetime import datetime
+from sys import version_info
+from time import time
+
+from config import (
+    ALIVE_IMG,
+    ALIVE_NAME,
+    BOT_NAME,
+    BOT_USERNAME,
+    GROUP_SUPPORT,
+    OWNER_NAME,
+    UPDATES_CHANNEL,
+)
+from program import __version__
+from driver.veez import user
+from driver.filters import command, other_filters
+from pyrogram import Client, filters
+from pyrogram import __version__ as pyrover
+from pytgcalls import (__version__ as pytover)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+__major__ = 0
+__minor__ = 2
+__micro__ = 1
+
+__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+
+
 @Client.on_message(filters.new_chat_members)
 async def new_chat(c: Client, m: Message):
     ass_uname = (await user.get_me()).username
