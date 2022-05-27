6be479a105c22f0b463c2b556a76a109a2a0624b
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-05-27 08:39:01 +0800

    Update updater.py

--- program/updater.py
+++ program/updater.py
@@ -53,7 +53,7 @@ def updater():
     return bool(changelog)
 
 
-@Client.on_message(command(["update", f"update@{BOT_USERNAME}"]) & ~filters.edited)
+@Client.on_message(command(["mupdate", f"mupdate@{BOT_USERNAME}"]) & ~filters.edited)
 @sudo_users_only
 async def update_repo(_, message: Message):
     chat_id = message.chat.id
@@ -67,7 +67,7 @@ async def update_repo(_, message: Message):
     await msg.edit("bot is **up-to-date** with [main](https://github.com/levina-lab/video-stream/tree/main)", disable_web_page_preview=True)
 
 
-@Client.on_message(command(["restart", f"restart@{BOT_USERNAME}"]) & ~filters.edited)
+@Client.on_message(command(["mrestart", f"mrestart@{BOT_USERNAME}"]) & ~filters.edited)
 @sudo_users_only
 async def restart_bot(_, message: Message):
     msg = await message.reply("`restarting bot...`")
